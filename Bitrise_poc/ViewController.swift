3efdb7799a624f1f5541eff2984f8384ff06fd62
Raunak Sinha <raunakrahul@gmail.com>
2019-05-15 16:16:33 +0530

    push 1

---
+++ Bitrise_poc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bitrise_poc
+//
+//  Created by TCS iMac on 15/05/19.
+//  Copyright © 2019 Woodside UX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bitrise_pocTests/Bitrise_pocTests.swift
@@ -0,0 +1,39 @@
+//
+//  Bitrise_pocTests.swift
+//  Bitrise_pocTests
+//
+//  Created by TCS iMac on 15/05/19.
+//  Copyright © 2019 Woodside UX. All rights reserved.
+//
+
+import XCTest
+@testable import Bitrise_poc
+
+class Bitrise_pocTests: XCTestCase {
+
+    let A = 10, B = 5
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testAdd(){
+        XCTAssertEqual(A + B, 15)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
