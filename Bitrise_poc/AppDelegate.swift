75a2f386f90ec7cb1fe122c268242a89ef196374
Raunak Sinha <raunakrahul@gmail.com>
2019-06-05 12:53:47 +0530

    test push

--- Bitrise_poc/AppDelegate.swift
+++ Bitrise_poc/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        MSAppCenter.start("f5ceef5e-ecf8-4c36-9b55-655dc08100b6", withServices:[
+            MSAnalytics.self,
+            MSCrashes.self
+            ])
+        
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Bitrise_poc' do
+
+  use_frameworks!
+
+pod 'AppCenter'
+
+
+end
